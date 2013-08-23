6e9738de2465818a492346ccb1586886d9651074
Tiago <tiago.joao+github@gmail.com>
2013-08-23 13:39:51 +0100

    Extract user ID from Desk v2 response's 'self' link

--- lib/desk/deash.rb
+++ lib/desk/deash.rb
@@ -49,7 +49,7 @@ module Hashie
     def id
       if includes_key_chain?("raw._links.self.href") || includes_key_chain?("_links.self.href")
         case self._links.self['class']
-        when 'case', 'phone_call', 'customer'
+        when 'case', 'phone_call', 'customer', 'user'
           self._links.self.href.split("/")[4].to_i
         end
       end
